cc09090b545af77a62562ff043867945f11534b1
guolong <guolong.deng@hand-china.com>
2023-10-09 18:38:53 +0800

    feat: client access session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+import User from "@/components/User";
 import { buttonVariants } from "@/components/ui/button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-const Home = () => {
+const Home = async() => {
+  const session = await getServerSession(authOptions);
+
   return (
     <div>
       <h1 className='text-xxl'>Home</h1>
       <Link className={buttonVariants()}  href='/admin'>Open My Admin</Link>
+    
+      <h2>Client Sesion</h2>
+      <User /> 
+      <h2>Server Sesion</h2>
+      {JSON.stringify(session)}
     </div>
   )
 }
